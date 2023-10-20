4d59ef3b2b65d763fddd0039b23d57048ed82006
kiyotone <kunwarkirtan36@gmail.com>
2023-10-21 00:25:38 +0545

    update:Form Error update

--- src/pages/Register/Form.jsx
+++ src/pages/Register/Form.jsx
@@ -25,42 +25,46 @@ const Form = () => {
 
             <form className='text-[.9rem]'>
               <div className='relative mt-[2rem]'>
-              <input placeholder=' ' value={values.name} onBlur={handleBlur} onChange={handleChange} type='text' id='name' className={`w-[25rem] h-[3rem] p-5 bg-gray-50 peer border-black placeholder-shown:border-gray-200 border rounded-full ${errors.name && touched.name &&  "border-red-600"}`}></input>
+              <input placeholder=' ' value={values.name} onBlur={handleBlur} onChange={handleChange} type='text' id='name' className={`w-[25rem] h-[3rem] p-5 bg-gray-50 peer border-black placeholder-shown:border-gray-200 border rounded-full ${errors.name && touched.name &&  "border-red-600 placeholder-shown:border-red-600"}`}></input>
               <label htmlFor='name' className='absolute bg-white top-[-.77rem] scale-[85%] text-black left-[6.28rem] peer-placeholder-shown:top-[.82rem] cursor-text ease-in-out duration-100 
                 peer-focus:text-black peer-placeholder-shown:text-gray-400 peer-placeholder-shown:scale-[100%]
                 peer-focus:top-[-.77rem] peer-focus:bg-white peer-focus:scale-[85%]
               '>NAME</label>
+              <p className='pt-1 text-red-600'>{errors?.name}</p>
               </div>
 
 
               
               <div className='relative mt-[2rem]'>
-              <input placeholder=' ' value={values.email} onBlur={handleBlur} onChange={handleChange} type='text' id='email' className={`w-[25rem] h-[3rem] p-5 bg-gray-50 peer border-black placeholder-shown:border-gray-200 border rounded-full ${errors.email && touched.email &&  "border-red-600"}`}></input>
+              <input placeholder=' ' value={values.email} onBlur={handleBlur} onChange={handleChange} type='text' id='email' className={`w-[25rem] h-[3rem] p-5 bg-gray-50 peer border-black placeholder-shown:border-gray-200 border rounded-full ${errors.email && touched.email &&  "border-red-600 placeholder-shown:border-red-600"}`}></input>
               <label htmlFor='email' className='absolute bg-white top-[-.77rem] scale-[85%] text-black left-[6.28rem] peer-placeholder-shown:top-[.82rem] cursor-text ease-in-out duration-100 
                 peer-focus:text-black peer-placeholder-shown:text-gray-400 peer-placeholder-shown:scale-[100%]
                 peer-focus:top-[-.77rem] peer-focus:bg-white peer-focus:scale-[85%]
               '>EMAIL</label>
+              <p className='pt-1 text-red-600'>{errors?.email}</p>
               </div>
 
 
               
               <div className='relative mt-[2rem]'>
-              <input placeholder=' ' value={values.password} onBlur={handleBlur} onChange={handleChange} type='text' id='password' className={`w-[25rem] h-[3rem] p-5 bg-gray-50 peer border-black placeholder-shown:border-gray-200 border rounded-full ${errors.password && touched.password &&  "border-red-600"}`}></input>
+              <input placeholder=' ' value={values.password} onBlur={handleBlur} onChange={handleChange} type='password' id='password' className={`w-[25rem] h-[3rem] p-5 bg-gray-50 peer border-black placeholder-shown:border-gray-200 border rounded-full ${errors.password && touched.password &&  "border-red-600 placeholder-shown:border-red-600"}`}></input>
               <label htmlFor='password' className='absolute bg-white top-[-.77rem] scale-[85%] text-black left-[6.28rem] peer-placeholder-shown:top-[.82rem] cursor-text ease-in-out duration-100 
                 peer-focus:text-black peer-placeholder-shown:text-gray-400 peer-placeholder-shown:scale-[100%]
                 peer-focus:top-[-.77rem] peer-focus:bg-white peer-focus:scale-[85%]
               '>PASSWORD</label>
+              <p className='pt-1 text-red-600'>{errors?.password}</p>
               </div>
 
 
               
               <div className='relative mt-[2rem]'>
-              <input placeholder=' ' value={values.mobile} onBlur={handleBlur} onChange={handleChange} type='text' id='mobile' className={`w-[25rem] h-[3rem] p-5 pl-[4rem] bg-gray-50 peer border-black placeholder-shown:border-gray ${errors.mobile && touched.mobile &&  "border-red-600"}-200 border rounded-full`}></input>
+              <input placeholder=' ' value={values.mobile} onBlur={handleBlur} onChange={handleChange} type='text' id='mobile' className={`w-[25rem] h-[3rem] p-5 pl-[4rem] bg-gray-50 peer border-black placeholder-shown:border-gray ${errors.mobile && touched.mobile &&  "border-red-600 placeholder-shown:border-red-600"}-200 border rounded-full`}></input>
               <label htmlFor='mobile' className='absolute bg-white top-[-.77rem] scale-[85%] text-black left-[6.28rem]  cursor-text ease-in-out
                 peer-focus:text-black peer-placeholder-shown:text-gray-400  peer-focus:bg-white
               '>MOBILE</label>
               <label htmlFor='mobile' className='absolute bg-white top-[.82rem] text-black left-[6.28rem]  cursor-text ease-in-out
               '>+977 </label>
+              <p className='pt-1 text-red-600'>{errors?.mobile}</p>
               </div>
 
               <button type='submit' className='mt-[2rem] w-[25rem] bg-[#ff4a26] hover:bg-[#d33920] rounded-full text-white' onClick={()=>handleSubmit} >REGISTER NOW</button>
