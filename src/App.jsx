4fb1cfb7eae56eb7de27ff6ca54f9de31f21e0c3
kiyotone <kunwarkirtan36@gmail.com>
2023-10-22 00:52:59 +0545

    Hero update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import Register from './pages/Register/index'
+import Hero from './components/Hero'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   <BrowserRouter>
     <div className='w-full h-full'>
       <Navbar />
+      <Hero />
       <Routes >
         
         <Route path='/register' element={ <Register /> } />
