56c9bb4c645d4f83269a25fbfcb8a02c73e7eb14
Pawan Awasthi <pawanawasthi481@gmail.com>
2023-10-26 16:21:58 +0545

    added single hostel page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
+import SingleHostelPage from "./pages/SingleHostelPage";
 
 function App() {
   return (
     <main>
       {/* <Login /> */}
       {/* <Card name="Boys Hostel" location="Kathmandu" price="15,000" /> */}
-      <Footer />
+      {/* <Footer /> */}
+      <SingleHostelPage />
     </main>
   );
 }
